a70ff18174da692008b8e8b0d1759cbc1e607f2f
Abdul Rehman <r3hmantom@gmail.com>
2023-04-13 19:42:33 +0500

    base variants

--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -2,15 +2,40 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 
+const containerVariant = {
+  initial: {
+    opacity: 0,
+    x: "100vw",
+  },
+  final: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: "spring",
+      delay: 0.5,
+    },
+  },
+};
+
+const nextVariant = {
+  hover: {
+    originX: 0,
+    scale: 1.3,
+    color: "#f8e112",
+    type: "spring",
+    stiffness: 300,
+  },
+};
+
 const Base = ({ addBase, pizza }) => {
   const bases = ["Classic", "Thin & Crispy", "Thick Crust"];
   const navigate = useNavigate();
 
   return (
     <motion.div
-      initial={{ x: "100vw" }}
-      animate={{ x: 0 }}
-      transition={{ type: "spring", delay: 0.5 }}
+      variants={containerVariant}
+      initial="initial"
+      animate="final"
       className="base container"
     >
       <h3>Step 1: Choose Your Base</h3>
@@ -19,13 +44,8 @@ const Base = ({ addBase, pizza }) => {
           let spanClass = pizza && pizza.base === base ? "active" : "";
           return (
             <motion.li
-              whileHover={{
-                originX: 0,
-                scale: 1.3,
-                color: "#f8e112",
-                type: "spring",
-                stiffness: 300,
-              }}
+              variants={nextVariant}
+              whileHover="hover"
               key={base}
               onClick={() => addBase(base)}
             >
