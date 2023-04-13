d8a5074dc77e7228039cc34db51253ef8364661a
Abdul Rehman <r3hmantom@gmail.com>
2023-04-13 20:16:25 +0500

    Orchestration, when, staggerChildren,mass,damping

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -11,8 +11,20 @@ const containerVariant = {
     x: 0,
     transition: {
       type: "spring",
-      delay: 0.5,
+      mass: 0.4,
+      damping: 8,
+      when: "beforeChildren",
+      staggerChildren: 0.4,
     },
+  },
+};
+
+const childVariants = {
+  initial: {
+    opacity: 0,
+  },
+  final: {
+    opacity: 1,
   },
 };
 const Order = ({ pizza }) => {
@@ -24,8 +36,10 @@ const Order = ({ pizza }) => {
       className="container order"
     >
       <h2>Thank you for your order :)</h2>
-      <p>You ordered a {pizza && pizza.base} pizza with:</p>
-      {pizza && pizza.toppings.map((topping) => <div key={topping}>{topping}</div>)}
+      <motion.p variants={childVariants}>You ordered a {pizza && pizza.base} pizza with:</motion.p>
+      <motion.div variants={childVariants}>
+        {pizza && pizza.toppings.map((topping) => <div key={topping}>{topping}</div>)}
+      </motion.div>
     </motion.div>
   );
 };
