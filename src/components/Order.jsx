6c70ed22a3e313dc81a1f083cc33017298662118
Abdul Rehman <r3hmantom@gmail.com>
2023-04-13 20:02:17 +0500

    Order Variant

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,13 +1,32 @@
 import React from "react";
+import { motion } from "framer-motion";
 
+const containerVariant = {
+  initial: {
+    opacity: 0,
+    x: "100vw",
+  },
+  final: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: "spring",
+      delay: 0.5,
+    },
+  },
+};
 const Order = ({ pizza }) => {
   return (
-    <div className="container order">
+    <motion.div
+      variants={containerVariant}
+      initial="initial"
+      animate="final"
+      className="container order"
+    >
       <h2>Thank you for your order :)</h2>
       <p>You ordered a {pizza && pizza.base} pizza with:</p>
-      {pizza &&
-        pizza.toppings.map((topping) => <div key={topping}>{topping}</div>)}
-    </div>
+      {pizza && pizza.toppings.map((topping) => <div key={topping}>{topping}</div>)}
+    </motion.div>
   );
 };
 
