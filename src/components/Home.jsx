e5938aaaebf732501483b141ddf90a195e4855ca
Abdul Rehman <r3hmantom@gmail.com>
2023-04-14 17:04:16 +0500

    repeatType mirror, repetition of keyframes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,9 +3,14 @@ import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 const buttonVariant = {
   hover: {
-    scale: [1, 1.1, 1, 1.1, 1, 1.1, 1],
+    scale: 1.1,
     textShadow: "0px 0px 8px rgb(255,255,255)",
     boxShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: {
+      duration: 0.3,
+      repeatType: "mirror",
+      repeat: Infinity,
+    },
   },
 };
 
