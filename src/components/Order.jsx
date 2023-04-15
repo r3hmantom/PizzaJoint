a1375c0334b064bb83bcc434a11dbb3daac840d2
Abdul Rehman <r3hmantom@gmail.com>
2023-04-15 19:16:27 +0500

    Removed Animate presence Order

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -28,8 +28,6 @@ const childVariants = {
   },
 };
 const Order = ({ pizza }) => {
-  const [showTitle, setShowTitle] = useState(true);
-
   setTimeout(() => {
     setShowTitle(false);
   }, 4000);
@@ -40,9 +38,7 @@ const Order = ({ pizza }) => {
       animate="final"
       className="container order"
     >
-      <AnimatePresence>
-        {showTitle && <motion.h2 exit={{ y: -1000 }}>Thank you for your order :)</motion.h2>}
-      </AnimatePresence>
+      <h2 exit={{ y: -1000 }}>Thank you for your order :)</h2>
       <motion.p variants={childVariants}>You ordered a {pizza && pizza.base} pizza with:</motion.p>
       <motion.div variants={childVariants}>
         {pizza && pizza.toppings.map((topping) => <div key={topping}>{topping}</div>)}
