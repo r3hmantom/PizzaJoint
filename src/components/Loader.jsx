77dd93b5fb5fc52cdf2fb9d204274e85b0c549ba
Abdul Rehman <r3hmantom@gmail.com>
2023-04-16 15:53:33 +0500

    Loader Variants

---
+++ src/components/Loader.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const loaderVariants = {
+  animationOne: {
+    x: [-20, 20],
+    y: [0, -30],
+    transition: {
+      x: { repeatType: "mirror", repeat: Infinity, duration: 0.5 },
+      y: { repeatType: "mirror", repeat: Infinity, duration: 0.25 },
+    },
+  },
+};
+
+const Loader = () => {
+  return (
+    <>
+      <motion.div variants={loaderVariants} animate="animationOne" className="loader"></motion.div>
+    </>
+  );
+};
+
+export default Loader;
