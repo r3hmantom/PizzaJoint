de45de9328a8d8dbe5f4eb71b35cc9fee5ed10ba
Abdul Rehman <r3hmantom@gmail.com>
2023-04-16 14:38:30 +0500

    Modal component

---
+++ src/components/Modal.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion";
+
+import React from "react";
+
+const backdrop = {
+  initial: {
+    opacity: 0,
+  },
+  final: {
+    opacity: 1,
+  },
+};
+
+function Modal({ showModal }) {
+  return (
+    <AnimatePresence mode="wait">
+      {showModal && (
+        <motion.div
+          variants={backdrop}
+          className="backdrop"
+          initial="initial"
+          animate="final"
+        ></motion.div>
+      )}
+    </AnimatePresence>
+  );
+}
+
+export default Modal;
