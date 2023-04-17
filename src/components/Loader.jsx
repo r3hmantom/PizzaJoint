4809b54342af0b4972e88fe071af1181ac6ee98c
Abdul Rehman <r3hmantom@gmail.com>
2023-04-17 14:00:17 +0500

    useCycle Hook

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,21 +1,35 @@
 import React from "react";
-import { motion } from "framer-motion";
+import { motion, useCycle } from "framer-motion";
 
 const loaderVariants = {
   animationOne: {
     x: [-20, 20],
     y: [0, -30],
     transition: {
+      type: "spring",
       x: { repeatType: "mirror", repeat: Infinity, duration: 0.5 },
+      y: { repeatType: "mirror", repeat: Infinity, duration: 0.25, ease: "easeOut" },
+    },
+  },
+  animationTwo: {
+    y: [0, -40],
+    x: 0,
+    transition: {
+      type: "spring",
       y: { repeatType: "mirror", repeat: Infinity, duration: 0.25, ease: "easeOut" },
     },
   },
 };
 
 const Loader = () => {
+  const [animation, cycleAnimation] = useCycle("animationOne", "animationTwo");
   return (
     <>
-      <motion.div variants={loaderVariants} animate="animationOne" className="loader"></motion.div>
+      <motion.div variants={loaderVariants} animate={animation} className="loader"></motion.div>
+
+      <div style={{ cursor: "pointer" }} onClick={() => cycleAnimation()}>
+        Cycle Loader
+      </div>
     </>
   );
 };
